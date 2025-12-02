1809b38434b5f09e88989ed6eabc1b14e4de1b2a
Saan R <148881010+saanrhyne@users.noreply.github.com>
2025-12-02 10:05:21 -0500

    Done

--- python/2a.py
+++ python/2a.py
@@ -4,19 +4,15 @@ with open("C:/Users/saanv/Downloads/input.txt", "r") as line:
   ids = line.readline().strip().split(",")
 
 sum = 0
-dupes = []
 
 for id in ids:
   start = int(id[:id.find("-")])
   stop = int(id[id.find("-") + 1:])
-  for i in range(start, stop): # iterate through each number in the ranges
-    if i not in dupes:
-      for j in range(1, len(str(i))//2): # create the chunk size
-        if i not in dupes:
-          for k in range(1, len(str(i))//2): # iterate through each chunk
-            if str(i)[k:k+j] == str(i)[k+j:k+j+j] and str(i)[k:k+j] != str(i)[k+j+j:k+j+j+j] and i not in dupes:
-              dupes.append(i)
-              sum += i
+  for i in range(start, stop + 1): # iterate through each number in the ranges
+    if str(i)[:len(str(i))//2] == str(i)[len(str(i))//2:]:
+      sum += i
 
-print(dupes)
-#print(sum)
+print(sum)
+
+# test 1: 20223751392, answer is too low, the range wasn't inclusive of the stop number
+# test 2: 20223751480, got it!
