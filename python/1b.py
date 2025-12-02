8d5f72bd73c0fa45211e10d8f0a8ec5b1cd4c2c0
Saan R <148881010+saanrhyne@users.noreply.github.com>
2025-12-02 11:02:22 -0500

    Yay it worked!

--- python/1b.py
+++ python/1b.py
@@ -1,5 +1,4 @@
-# yeah idk
-import math
+# This is the bane of my existence btw 
 
 pos = 50
 count = 0
@@ -8,15 +7,20 @@ with open("C:/Users/saanv/Downloads/input.txt", "r") as lines:
   for line in lines:
     val = int(line[1:])
     if line[0] == "R":
-      if pos + val % 100 > 99: count += 1
-      count += val // 100
-      pos = (pos + val) % 100
+      for i in range(val):
+        pos += 1
+        if pos == 100:
+          pos = 0
+          count += 1
     else:
-      if val % 100 > 0 and val % 100 > pos: count += 1
-      count += val // 100
-      pos = (pos - val) % 100
+      for i in range(val):
+        pos -= 1
+        if pos == -1:
+          pos = 99
+        if pos == 0:
+          count += 1
 
 print(count)
 
-# test 1: 7426, not right, now I have to wait 10 whole minutes
-# test 2: 6967, but I know that's not right from before
+# attempt 1: 7004, it's not right, I think I was adding 1 to the range but that's actually not right
+# attempt 2: 6932, I've never got that value before, but I still have to wait 3 mins 3 seconds, IT'S RIGHT LETS GOOOOOOOOO
